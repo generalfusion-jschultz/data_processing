607579ec517aeda903efdb19ccb13400f5541ab3
Jason Schultz <jason.schultz@generalfusion.com>
2024-01-23 15:21:21 -0800

    Added new classes

--- src/data_processing/__init__.py
+++ src/data_processing/__init__.py
@@ -4,3 +4,5 @@ from data_processing.data_processing import Shot
 from data_processing.data_processing import Shots
 from data_processing.data_processing import Measurement
 from data_processing.data_processing import Measurements
+from data_processing.data_processing import DataPoint
+from data_processing.data_processing import DataList
